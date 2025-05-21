bf09ae53429ac635b206f2182f563aab18862714
FulgrimDragon <39089386+FulgrimDragon@users.noreply.github.com>
2025-05-21 23:05:11 +0100

    menu system works with controller

---
+++ Earth Shard/Assets/Scripts/Menu/FirstSelected.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
+
+public class FirstSelected : MonoBehaviour
+{
+    [SerializeField] private EventSystem _eventSystem;
+    [SerializeField] private GameObject _firstSelectedObj;
+
+    private void OnEnable()
+    {
+        _eventSystem.SetSelectedGameObject(_firstSelectedObj);
+    }
+
+}
